a17aaaad91d64d9b762ed224145269e053362b70
Sterling Deng <sterlingdeng@gmail.com>
2019-05-20 02:27:58 -0700

    test coverage for studentCollection

--- internal/collections/collections_test.go
+++ internal/collections/collections_test.go
@@ -1,9 +1,47 @@
 package collections
 
 import (
-	"sync"
+	"reflect"
 	"testing"
 )
+
+var mockExamsSlice = []*ExamData{
+	&ExamData{
+		Exam:      1,
+		StudentID: "s1",
+		Score:     50.00,
+	},
+	&ExamData{
+		Exam:      2,
+		StudentID: "s2",
+		Score:     100.00,
+	},
+	&ExamData{
+		Exam:      3,
+		StudentID: "s3",
+		Score:     75.00,
+	},
+	&ExamData{
+		Exam:      3,
+		StudentID: "s2",
+		Score:     33.33,
+	},
+}
+
+var studentCollection = StudentCollection{
+	Data: make(map[string][]*ExamData),
+}
+
+var examCollection = ExamCollection{
+	Data: make(map[int32][]*ExamData),
+}
+
+func init() {
+	for _, exam := range mockExamsSlice {
+		studentCollection.addData(exam)
+		examCollection.addData(exam)
+	}
+}
 
 func Test_calculateAverageExamScore(t *testing.T) {
 	type args struct {
@@ -14,7 +52,13 @@ func Test_calculateAverageExamScore(t *testing.T) {
 		args args
 		want float32
 	}{
-		// TODO: Add test cases.
+		{
+			name: "test 1",
+			args: args{
+				exams: mockExamsSlice,
+			},
+			want: 64.582504,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -26,53 +70,91 @@ func Test_calculateAverageExamScore(t *testing.T) {
 }
 
 func TestStudentCollection_addData(t *testing.T) {
-	type fields struct {
-		Data map[string][]*ExamData
-		mu   sync.Mutex
-	}
-	type args struct {
-		examData *ExamData
-	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		name      string
+		studentID string
+		want      bool
 	}{
-		// TODO: Add test cases.
+		{
+			name:      "test1",
+			studentID: "s1",
+			want:      true,
+		},
+		{
+			name:      "test2",
+			studentID: "s2",
+			want:      true,
+		}, {
+			name:      "test3",
+			studentID: "s0",
+			want:      false,
+		}, {
+			name:      "test4",
+			studentID: "s3",
+			want:      true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := &StudentCollection{
-				Data: tt.fields.Data,
-				mu:   tt.fields.mu,
+			if _, ok := studentCollection.Data[tt.studentID]; ok != tt.want {
+				t.Errorf("Looking up %v in StudentCollection.Data,, want %v, got %v", tt.studentID, tt.want, ok)
 			}
-			s.addData(tt.args.examData)
 		})
 	}
 }
 
-func TestExamCollection_addData(t *testing.T) {
-	type fields struct {
-		Data map[int32][]*ExamData
-		mu   sync.Mutex
-	}
-	type args struct {
-		examData *ExamData
-	}
+func TestStudentCollection_GetList(t *testing.T) {
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		name string
+		want []string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "test1",
+			want: []string{"s1", "s2", "s3"},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			e := &ExamCollection{
-				Data: tt.fields.Data,
-				mu:   tt.fields.mu,
+			if got := studentCollection.GetList(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("StudentCollection.GetList() = %v, want %v", got, tt.want)
 			}
-			e.addData(tt.args.examData)
+		})
+	}
+}
+
+func TestExamCollection_GetByID(t *testing.T) {
+	tests := []struct {
+		name      string
+		studentID string
+		want      *AggregatedExams
+	}{
+		{
+			name:      "test1",
+			studentID: "s10",
+			want:      nil,
+		},
+		{
+			name:      "test2",
+			studentID: "s1",
+			want: &AggregatedExams{
+				Exams:   []*ExamData{mockExamsSlice[0]},
+				Average: 50.00,
+			},
+		},
+		{
+			name:      "test3",
+			studentID: "s2",
+			want: &AggregatedExams{
+				Exams:   []*ExamData{mockExamsSlice[1], mockExamsSlice[3]},
+				Average: (100.00 + 33.33) / 2.0,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := studentCollection.GetByID(tt.studentID); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ExamCollection.GetByID() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
